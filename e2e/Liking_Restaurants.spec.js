8fd11d3dcc564035c97aa84a8e56a932e97538b7
Rizqi Muhammad Khatami Sandi <95211808+mksdesuyo@users.noreply.github.com>
2022-05-15 12:27:54 +0700

    Add files via upload

---
+++ e2e/Liking_Restaurants.spec.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+
+Feature('Liking Restaurant');
+
+Before(({I}) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked menu restaurant', ({I}) => {
+  I.dontSeeElement('.restaurant-item');
+});
+
+Scenario('liking one restaurant', async ({I}) => {
+  I.dontSeeElement('.restaurant-item');
+  I.amOnPage('/');
+  I.seeElement('.restaurant__title a');
+
+  const firstRestaurant = locate('.restaurant__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.restaurant-item');
+  const likedRestaurantTitle = await I.grabTextFrom('.restaurant__title');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+});
